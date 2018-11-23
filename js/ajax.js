04acf25f7e40e83eb55ba50a859f6b638040e51c
Srinivas Shekar <srinivasshekar01@gmail.com>
2018-11-23 17:36:23 +0530

    Updated js funcs

--- js/ajax.js
+++ js/ajax.js
@@ -23,9 +23,51 @@ getCategory = function(name) {
 };
 
 getMenu = function(name) {
+    $.get("https://piedp.herokuapp.com/menu",
+        {
+            "name": getRestaurants()
+        },
+        false,
+        function(data){
+            console.log(data)
+            // name=data
+            // console.log(name)
+            return data;
+        });
 
+    
 };
 
 getCategoryContent = function(name,category) {
-
+    $.get("https://piedp.herokuapp.com/categorycontent",
+    {
+        "name": getRestaurants(),
+        "category":getCategory(name)
+    },
+    false,
+    function(data){
+        console.log(data)
+        // name=data
+        // console.log(name)
+        return data;
+    });
 };
+
+getPrice = function(name,item) {
+	var price=0;
+	$.ajax({
+	    url: "https://piedp.herokuapp.com/price",
+	    dataType: 'json',
+	    data: {
+			"restaurant":name,
+			"item":item,
+		},
+	    async: false,
+	    success: function(data){
+			if(data["price"]!="NF") {
+				price = data["price"]
+			}
+	    }
+	});
+	return price;
+}
