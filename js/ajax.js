47b7f0a2b1ac6c9906c9fee6b1dd05dcb32ec169
Srinivas Shekar <srinivasshekar01@gmail.com>
2018-11-23 13:26:37 +0530

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,9 +1,25 @@
 getRestaurants = function() {
-
+    $.get("http://localhost:5000/restaurants",
+        {
+        },
+        false,
+        function(data){
+            console.log(data)
+            return data;
+        });
+        
 }
 
 getCategory = function(name) {
-
+    $.get("http://localhost:5000/category",
+        {
+            "name": getRestaurants()
+        },
+        false,
+        function(data){
+            console.log(data)
+            return data;
+        });
 }
 
 getMenu = function(name) {
